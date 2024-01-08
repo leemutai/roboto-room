eb581f8ebeb2ebb6172ba4ac6cc9533c41c9d1de
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-01-08 16:46:59 +0300

    first commit

---
+++ app/src/main/java/com/example/login_and_register_app_kotlin_room/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.login_and_register_app_kotlin_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
